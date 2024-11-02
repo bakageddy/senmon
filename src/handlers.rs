d1e323103a989814a4d2e29270d6af9aaed7f6b1
Dinesh Kumar G <gdineshda@gmail.com>
2024-11-02 20:55:52 +0530

    Add report

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,17 +1,29 @@
 use std::io::Read;
-
-use axum::{
-    response::Html,
-    extract::Multipart,
-    http::StatusCode,
-    Form,
-};
+use axum::{http::StatusCode, response::Html, Form};
 
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct DownloadReq {
     file_name: String,
+}
+
+pub struct UploadFileReq<'a> {
+    file_name: &'a str,
+    file_contents: &'a str,
+    password: &'a str,
+    salt: &'a str,
+}
+
+impl<'a> UploadFileReq<'a> {
+    pub fn new() -> Self {
+        Self {
+            file_name: "",
+            file_contents: "",
+            password: "",
+            salt: "",
+        }
+    }
 }
 
 pub async fn home() -> Html<String> {
@@ -39,16 +51,22 @@ pub async fn download_file(
     Ok(data)
 }
 
-pub async fn upload_file(mut form_input: Multipart) -> StatusCode {
+pub async fn upload_file(mut form_input: axum::extract::Multipart) -> StatusCode {
+    let mut file_name: String;
+    let mut file_contents: String;
+    let mut password: String;
     while let Ok(Some(field)) = form_input.next_field().await {
         let field_name = field.name();
         if field_name.is_none() {
             return StatusCode::BAD_REQUEST;
         }
         if field_name.unwrap() == "file" {
-            let file_name = field.file_name().unwrap_or("default_file_name").to_string();
-            let file_contents = field.text().await.unwrap();
-            std::fs::write(file_name, file_contents).unwrap();
+            file_name = field.file_name().unwrap_or("default_file_name").to_string();
+            file_contents = field.text().await.unwrap();
+        } else if field_name.unwrap() == "pwd" {
+            password = field.text().await.expect("Required: password").to_string();
+        } else {
+            return StatusCode::BAD_REQUEST;
         }
     }
     StatusCode::OK
